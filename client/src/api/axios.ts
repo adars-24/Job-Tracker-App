4115bf6780302415283f1e375c7ad47ffe463ac1
Adarsh Singh <adarshsinghciaz@gmail.com>
2026-05-02 01:39:11 +0530

    changed axios file

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -1,7 +1,6 @@
 import axios , { type AxiosInstance } from "axios";
-const api : AxiosInstance = axios.create({
-      baseURL: "http://localhost:5000/api",
-
+const api: AxiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api"
 })
 
 api.interceptors.request.use((config) => {
